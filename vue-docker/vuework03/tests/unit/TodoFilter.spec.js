c3a7c992e5a15add12c0d75f96661a51c7ea38f3
Yoshi-purple <n7ydahew@gmail.com>
2021-08-21 16:03:34 +0900

    【Vueテスト課題①レビュー依頼】修正しました。

--- vue-docker/vuework03/tests/unit/TodoFilter.spec.js
+++ vue-docker/vuework03/tests/unit/TodoFilter.spec.js
@@ -2,9 +2,11 @@ import { mount } from "@vue/test-utils"
 import TodoFilter from "../../src/views/TodoFilter.vue"
 
 
+const wrapper = mount(TodoFilter)
 test("filtertrigger", async () => {
-  const wrapper = mount(TodoFilter)
   await wrapper.find ('input').setValue("all")
   await wrapper.find("input").trigger("click")
+})
+it('全てのタスクが表示されること', () => {
   expect(wrapper.emitted("showSwitch","all"))
 })
--- vue-docker/vuework03/tests/unit/TodoInput.spec.js
+++ vue-docker/vuework03/tests/unit/TodoInput.spec.js
@@ -1,15 +1,19 @@
 import { mount } from "@vue/test-utils"
 import TodoInput from "../../src/views/TodoInput.vue"
 
+const wrapper = mount(TodoInput)
+const input = wrapper.find('[data-test="newTodo"]')
 test("set to input", async () => {
-  const wrapper = mount(TodoInput)
-  const input = wrapper.find('[data-test="newTodo"]')
   await input.setValue("タスク")
-  expect(input.element.value).toBe("タスク")
 });
+it('追加するタスクがinputに登録されること', () => {
+  expect(input.element.value).toBe("タスク")
+})
 
 test("trigger", async () => {
-  const wrapper = mount(TodoInput);
+  // const wrapper = mount(TodoInput);
   await wrapper.get('[data-test="addTodo"]').trigger("click")
-  expect(wrapper.emitted("add"))
 });
+it('クリックイベントが設定されていること', () => {
+  expect(wrapper.emitted("add"))
+})
